646010374f784f9764d61583538ba095eb278422
MRKmovies <karthickmrkom@gmail.com>
2024-10-02 12:29:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -119,7 +119,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "https://nice-meris-mahaan.koyeb.app/")
+URL = environ.get("URL", "persistent-veronike-karthick0097-8d9b7675.koyeb.app/")
 if len(URL) == 0:
     print('Error - URL is missing, exiting now')
     exit()
